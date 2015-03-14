42345ecf66dc685e957e6144c19d770f1bc4ca4a
Your Name <you@example.com>
2015-03-14 14:39:17 +0000

    improvement: Now saves a formatted.xml file with HTML content inside the Dynarex document.

--- lib/liveblog.rb
+++ lib/liveblog.rb
@@ -5,6 +5,7 @@
 require 'time'
 require 'dynarex'
 require 'fileutils'
+require 'martile'
 
 class LiveBlog
 
@@ -16,9 +17,7 @@ class LiveBlog
     @t = Time.now
     dxfile = File.join(path(), 'index.xml')
 
-    puts 'fxfile: ' + dxfile.inspect
     File.exists?(dxfile) ? @dx = Dynarex.new(dxfile) : new_file()      
-    puts '@dx : ' + @dx.inspect
       
   end
 
@@ -41,15 +40,18 @@ EOF
     
   end
   
-  def add_entry(hashtag, entry)
+  def add_entry(hashtag, raw_entry)
     
+    entry = String.new(raw_entry)
     a =  @dx.all
     rec = a.find {|section| section.x.lstrip.lines.first =~ /#\s*#{hashtag}/}
     
     if rec then
-      rec.x += "\n" + entry
+      
+      hashtag = entry.slice!(/#\w+$/)      
+      rec.x += "\n" + entry.rstrip
       save()      
-      hashtag = entry[/#\w+$/]
+
       [true, "%s %s/%s/index.html%s" % [entry, @urlbase, path(), hashtag]]
     else
       [false, 'rec not found']
@@ -69,6 +71,7 @@ EOF
     
     @dx.save File.join(path(), 'index.xml')
     File.write File.join(path(), 'index.txt'), @dx.to_s
+    save_html()
   end
   
   private
@@ -77,6 +80,24 @@ EOF
 
     File.join [@t.year.to_s, Date::MONTHNAMES[@t.month].downcase[0..2], \
                                                                 @t.day.to_s]
+  end
+  
+  def save_html()
+    
+    newfilepath = File.join(path(), 'formatted.xml')
+    FileUtils.cp File.join(path(), 'index.xml'), newfilepath
+    
+    doc = Rexle.new File.read(newfilepath)
+    
+    doc.root.xpath('records/section/x') do |x|
+       html = RDiscount.new(Martile.new(x.text).to_html).to_html
+       e = x.parent
+       x.delete
+       e.add Rexle.new("<x>%s</x>" % html).root
+    end
+
+    File.write newfilepath, doc.xml(pretty: true)
+    
   end
 
   def ordinalize(n)
